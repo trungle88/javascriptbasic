9afbff989215ba3d0a859d5bd8e792891763c873
Trung Xabi <thanhtrungnait88@gmail.com>
2018-06-07 01:11:31 +0700

    update js

--- Example_1/custom.js
+++ Example_1/custom.js
@@ -1,1 +1,16 @@
-alert("Hello world in extenal file");
+// alert("Hello world in extenal file");
+// alert(1);
+// alert(true);
+
+/*
+var result = confirm("Are you sure to delete this record?");
+if (result == true) {
+	alert("user accepted");
+}
+else{
+	alert("user rejected");
+}
+*/
+
+var input = prompt("please tell me your name?","No Name");
+alert(input);
